1c6a9f2f477c4a78fc3b83a153212e86ac3a988b
louis <louisbamidele@icloud.com>
2023-09-04 05:06:40 +0100

    helpppp jessu jesus hhh llll

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Target must be serverless
-  serverless: true,
-  distDir: "build",
   images: {
     domains: ["lh3.googleusercontent.com"], // allowed domains here
   },
